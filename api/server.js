08b5f0d843358fd1f19609eda81ef19ec7b3972f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-09 16:03:07 +0100

    ketchup script

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const UserRouter = require("../users/user-router.js");
+const UserRouter = require("./users/user-router.js");
 
 const server = express();
 
--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const db = require("../data/db-config.js");
+const db = require("../../data/db-config.js");
 
 const router = express.Router();
 
