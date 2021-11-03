29f4ca1fbeed978e7383b0b346ae31c827de935f
Ladrillo <gabriel@cabrejas.net>
2021-11-03 14:40:37 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -5,7 +5,6 @@ const UserRouter = require('./users/user-router.js')
 const server = express()
 
 server.use(express.json())
-
 server.use('/api/users', UserRouter)
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message })
