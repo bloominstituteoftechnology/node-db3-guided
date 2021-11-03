cc559901db5117b56e6d15936ce8ee30312b1a16
Ladrillo <gabriel@cabrejas.net>
2021-11-03 13:44:05 +0100

    remove helmet for clarity (too many headers)

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const helmet = require('helmet')
 
 const UserRouter = require('./users/user-router.js')
 
 const server = express()
 
-server.use(helmet())
 server.use(express.json())
 
 server.use('/api/users', UserRouter)
