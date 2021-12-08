16ad89423bc31fe7e24b3919c9b1353b9ebf41ff
Ladrillo <gabriel@cabrejas.net>
2021-12-08 16:40:58 +0100

    remove update

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -5,7 +5,6 @@ module.exports = {
   find,
   findById,
   add,
-  update,
   remove
 }
 
@@ -68,15 +67,6 @@ function add(user) {
   return db('users')
     .insert(user)
     .then(([id]) => { // eslint-disable-line
-      return findById(id)
-    })
-}
-
-function update(changes, id) {
-  return db('users')
-    .where({ id })
-    .update(changes)
-    .then(count => {
       return findById(id)
     })
 }
